53c2a4f07f3fbce4c16535c6540dd2a1bb61b297
PataZik <alz22076@spengergasse.at>
2024-06-04 13:40:45 +0200

    Add files via upload

---
+++ schleife.js
@@ -0,0 +1,15 @@
+var twoSum = funktion(nums, target) {
+    for (i=0;i<nums.lengt-length;i++){
+        for (j=i+1; j<nums.lenght; j++) {
+            if (nums[i] + nums(j) == target){
+                return [i,j];
+            }
+        }
+    }
+}
+
+let bspl = {
+    nums: [2, 7, 11, 15],
+    target: 26,
+    answer: [2, 3]
+}
